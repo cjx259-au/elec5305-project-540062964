a88fd3bdabf053c929cebc4b6846770622f75b9b
cjx259-au <cjx2590805@gmail.com>
2025-11-16 23:48:42 +1100

    Delete truepeak_dbTP.m

--- truepeak_dbTP.m
+++
@@ -1,7 +0,0 @@
-function tp = truepeak_dbTP(x, Fs, cfg)
-% 过采样估计真峰值（dBTP）
-os  = cfg.truePeakOversample; 
-y   = resample(x, os, 1);              % 简单上采样（足够教学使用）
-peak= max(abs(y));
-tp.dbTP = 20*log10(max(peak, eps));
-end
